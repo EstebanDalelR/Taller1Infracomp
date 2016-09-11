7bc256f236b096f3f75e40bed6101bac1cb8c0e8
Juan Pablo Arevalo <Juan Pablo Arevalo>
2016-09-11 16:29:08 -0500

    clases nuevas, msj acabado

--- src/buffer/Buffer.java
+++ src/buffer/Buffer.java
@@ -2,4 +2,19 @@ package buffer;
 
 public class Buffer {
 
+	public int capacidad;
+	public int nMensajes;
+	public boolean lleno;
+	public [] mensaje;
+	public boolean synchronized estoyLleno()
+	{
+		if()
+	}
+	if buffer is full  
+	
+	public Buffer(int capacidadn, boolean llenon, {
+		capacidad = ncapacidad;
+
+	}
+
 }
---
+++ src/buffer/Mensaje.java
@@ -0,0 +1,22 @@
+package buffer;
+
+public class Mensaje
+{
+	public long msj; 
+	public Mensaje(long msjn)
+	{
+		msj = msjn;
+	}
+	public void dormir()
+	{
+		wait();
+	}
+	public void actualizarInfo(int info)
+	{
+		msj = info;
+	}
+	public void despertar()
+	{
+		notify();
+	}
+}
