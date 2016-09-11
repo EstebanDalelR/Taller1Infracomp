744024b0ee9d4696864616dc13d9e4035dc0bcb6
Esteban Dalel R <estebandalelr@gmail.com>
2016-09-11 16:39:41 -0500

    Agregar dejarMensaje()

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -4,6 +4,19 @@ public class Cliente extends Thread {
 
 	public Mensaje mensajexdxd;
 	public Buffer bofer;
-	
+
+	public boolean dejarMensaje(Mensaje mensajexdxd){
+
+		if (bofer.recibir(mensajexdxd)) {
+			wait();
+		}	
+		else{
+			while(!bofer.recibir(mensajexdxd))
+			{
+				dejarMensaje(mensajexdxd);
+			}
+		}
+	}
+
 
 }
