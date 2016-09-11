2983c859f20b9803e402f14c123f2c26b5391a90
Alejandro Tovar <didage@gmail.com>
2016-09-11 16:55:20 -0500

    Boceto del servidor

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,10 +1,26 @@
 package servidor;
-
+import buffer.*;
 public class Servidor extends Thread {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private Buffer bf;
+	
+	public Servidor(Buffer bf) {
+		this.bf=bf;
 	}
 
+	public void run() {
+		while(true) {
+			Mensaje msn = null;
+			do {
+				msn = bf.atender();
+			}while(msn==null);
+			procesar(msn);
+		}
+	}
+	
+	public void procesar(Mensaje msn) {
+		msn.actualizarInfo(msn.darInfo()++);
+		sleep(20);
+		msn.despertar();
+	}
 }
