caabbe5f16102f8e90db31b014b53d98f700c73d
Esteban Dalel R <estebandalelr@gmail.com>
2016-09-11 20:45:34 -0500

    Comentarios

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -14,31 +14,38 @@ public class Cliente extends Thread {
 	private ArrayList<Mensaje> mensajesProcesados; 
 	private long solicitudes;
 	private Mensaje msn;
-	
+
+	//constructor
 	public Cliente(long numSolicitudes, Buffer bf) {
 		solicitudes=numSolicitudes;
 		mensajesProcesados=new ArrayList<>();
 		bofer=bf;
 	}
-
-	public void dejarMensaje(Mensaje mensajexdxd){
-			msn = mensajexdxd;
-			while(!bofer.recibir(mensajexdxd, this))yield();
-			mensajesProcesados.add(mensajexdxd);
-			solicitudes--;
-	}
 	
+	//deja un mensaje en el buffer
+	public void dejarMensaje(Mensaje mensajexdxd){
+		msn = mensajexdxd;
+		//espera activa para entregar mensaje
+		while(!bofer.recibir(mensajexdxd, this))
+			yield();
+		//al despertar agrega el mensaje procesado
+		mensajesProcesados.add(mensajexdxd);
+		solicitudes--;
+	}
+
+	//ejecución
 	public void run() {
+		//mientras queden solicitudes por atender, lo intenta
 		while(solicitudes>0) {
 			dejarMensaje(new Mensaje(solicitudes));
 			System.out.println("dejo una solicitud");
 		}
 	}
-	
+
 	public int arraySize() {
 		return mensajesProcesados.size();
 	}
-	
+
 	public Mensaje getMessage() {
 		return msn;
 	}
--- src/main/Main.java
+++ src/main/Main.java
@@ -16,48 +16,55 @@ import servidor.*;
 
 
 public class Main {
-	
+	//crea la lista de clientes
 	private static ArrayList<Cliente> clientes;
+	//crea la lista de servidores
 	private static ArrayList<Servidor> servidores;
-	
+
 	public static void main(String[] args) {
-		//tomado de https://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example
 		
+		//lee el archivo propiedades		
 		ArrayList<Integer> a = new ArrayList();
 		try 
 		{
 			BufferedReader br = new BufferedReader(new FileReader("./src/main/propiedades.txt"));
 			System.out.println("leido del archivo");
-		String sCurrentLine;
+			String sCurrentLine;
 
-		while ((sCurrentLine = br.readLine()) != null) {
-		int l = Integer.parseInt(sCurrentLine);
-		       a.add(l);       
-		}
+			while ((sCurrentLine = br.readLine()) != null) {
+				int l = Integer.parseInt(sCurrentLine);
+				a.add(l);       
+			}
 
 		} catch (IOException e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		}
-		
+
+		//guarda los datos de propiedades
 		int numServ = a.get(0);
 		int capBuffer = a.get(1);
 		int numClientes = a.get(2);
 		int numMensajes = a.get(3);
 		System.out.println("recibido parametro");
+
+		//crea el buffer
 		Buffer bf = new Buffer(capBuffer, numClientes);
 		
+		//crea las listas de servidores y clientes
 		clientes = new ArrayList<>();
 		servidores = new ArrayList<>();
 		
+		//crea los clientes
 		for(int i=0; i<numClientes; i++) {
 			Cliente actual=new Cliente(numMensajes, bf);
 			actual.start();
 			clientes.add(actual);
 		}
-		
+
 		System.out.println("creados servidores");
-		//while(true)
 		System.out.println("creados clientes");
+		
+		//crea los servidores
 		for(int i=0; i<numServ;i++) {
 			Servidor actual=new Servidor(bf);
 			actual.start();
